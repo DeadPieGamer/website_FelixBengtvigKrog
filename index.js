a8eee611892b22f90d0ec038f39a38b821736c21
DeadPieGamer <felix@krog.dk>
2025-06-12 04:59:27 +0200

    Tic-tac-toe and updated looks of all but portfolio

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+import { readFile, writeFile } from "node:fs";
+
 const app = express();
 const port = 3000;
 
@@ -10,6 +12,8 @@ app.use("/jquery", express.static(__dirname + "/node_modules/jquery/dist/"));
 app.use("/assets", express.static("assets"));
 app.use("/css", express.static("css"));
 app.use("/js", express.static("js"));
+
+app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
@@ -29,6 +33,40 @@ app.get("/projects/personal/retrace", (req, res) => {
 
 app.get("/projects/personal/web-game", (req, res) => {
   res.sendFile(__dirname + "/public/projects/personal/web-game.html");
+});
+
+// Saves a move into the current game
+app.post("/projects/personal/web-game", (req, res) => {
+  console.log("JSON-ifying: " + req.body);
+  writeFile(
+    __dirname + "/assets/game-demos/tic-tac-toe/game-board.json",
+    JSON.stringify(req.body),
+    "utf8",
+    () => {
+      console.log("finished writing file");
+    }
+  );
+  //Do something with the data
+  // console.log(req.body);
+  res.sendStatus(200);
+});
+
+// Gives the current state of the game JSON file
+app.get("/projects/personal/web-game/current", (req, res) => {
+  readFile(
+    __dirname + "/assets/game-demos/tic-tac-toe/game-board.json",
+    "utf8",
+    (err, data) => {
+      if (!err) {
+        console.log("I got data: " + data);
+        res.json(data);
+        console.log("data sent");
+      } else {
+        console.log("ohnoes, got error: " + err);
+        res.sendStatus(404);
+      }
+    }
+  );
 });
 
 app.get("/projects/uni/killeronboard", (req, res) => {
---
+++ js/ttt-script.js
@@ -0,0 +1,178 @@
+const timeBetweenMoves = 1000 * 60 * 5; // In milliseconds. This should be equivalent to 5 minutes
+
+$(".ttt-box").on("click", function (evt) {
+  doMove($(this));
+});
+
+$("#ttt-reset-button").on("click", function (evt) {
+  resetGame();
+});
+
+function resetGame() {
+  console.log("Game reset");
+  saveBoard(new TttBoard());
+  loadBoard();
+}
+
+function TttBoard() {
+  return {
+    row: [
+      {
+        column: ["", "", ""],
+      },
+      {
+        column: ["", "", ""],
+      },
+      {
+        column: ["", "", ""],
+      },
+    ],
+    nextMove: "x",
+    lastMoment: 0,
+  };
+}
+
+// Bad code right here, but I was quite stressed with other stuff when writing it, and didn't wanna return to fix something that ain't broke later
+function figureOutBox(box) {
+  let splitBox = box.attr("class").split(" ");
+  let thisLocation = [];
+  switch (splitBox[1]) {
+    case "ttt-r1":
+      thisLocation.push(0);
+      break;
+    case "ttt-r2":
+      thisLocation.push(1);
+      break;
+    default:
+      thisLocation.push(2);
+      break;
+  }
+
+  switch (splitBox[2]) {
+    case "ttt-c1":
+      thisLocation.push(0);
+      break;
+    case "ttt-c2":
+      thisLocation.push(1);
+      break;
+    default:
+      thisLocation.push(2);
+      break;
+  }
+
+  return thisLocation;
+}
+
+async function doMove(whichBox) {
+  let currentTime = new Date().getTime();
+
+  let currentGameBoard = new TttBoard();
+
+  // let boardRequest = $.get("/projects/personal/web-game/current");
+
+  $.get("/projects/personal/web-game/current", (data) => {
+    console.log("Found following existing game: " + data);
+    let parsedData = JSON.parse(data);
+
+    // Attempt to rebuild structure as current parse states that row doesn't exist
+    for (let i = 0; i < currentGameBoard.row.length; i++) {
+      currentGameBoard.row[i] = { column: parsedData[`row[${i}][column][]`] };
+    }
+    currentGameBoard.nextMove = parsedData.nextMove;
+    currentGameBoard.lastMoment = parsedData.lastMoment;
+  })
+    .fail(() => {
+      console.log("Found no existing game");
+    })
+    .always(() => {
+      // If enough time hasn't passed, return
+      let timeDif =
+        currentGameBoard.lastMoment - currentTime;
+
+      timeDif += timeBetweenMoves;
+
+      console.log(
+        "I am at time: " +
+          currentTime +
+          ", which is " +
+          (timeDif) +
+          " timeDif. With timeBetweenMoves added: " +
+          (timeDif + timeBetweenMoves)
+      );
+
+      if (timeDif > 0) {
+        alert(
+          `You moved too quickly! There's still: ${Math.floor(
+            timeDif / 60.0 / 1000.0
+          )} minutes and ${Math.floor(
+            timeDif / 1000.0 % 60
+          )} seconds left until the next possible move.`
+        );
+        return;
+      }
+
+      let imageBox = whichBox.children();
+      imageBox.attr(
+        "src",
+        "/assets/game-demos/tic-tac-toe/" + currentGameBoard.nextMove + ".svg"
+      );
+      imageBox.attr("alt", currentGameBoard.nextMove);
+      imageBox.removeClass("d-none");
+
+      let boxCoordinates = figureOutBox(whichBox);
+
+      currentGameBoard.row[boxCoordinates[0]].column[boxCoordinates[1]] =
+        currentGameBoard.nextMove;
+
+      currentGameBoard.nextMove = currentGameBoard.nextMove === "x" ? "o" : "x";
+      currentGameBoard.lastMoment = currentTime;
+
+      saveBoard(currentGameBoard);
+    });
+}
+
+function saveBoard(boardObj) {
+  $.post("/projects/personal/web-game", boardObj);
+}
+
+function loadBoard() {
+  let currentGameBoard = new TttBoard();
+
+  // let boardRequest = $.get("/projects/personal/web-game/current");
+
+  $.get("/projects/personal/web-game/current", (data) => {
+    let parsedData = JSON.parse(data);
+
+    // Attempt to rebuild structure as current parse states that row doesn't exist
+    for (let i = 0; i < currentGameBoard.row.length; i++) {
+      currentGameBoard.row[i] = { column: parsedData[`row[${i}][column][]`] };
+    }
+  })
+    .fail(() => {
+      console.log("Found no existing game");
+    })
+    .always(() => {
+      for (let i = 0; i < currentGameBoard.row.length; i++) {
+        for (let j = 0; j < currentGameBoard.row[0].column.length; j++) {
+          let imageBox = $(`.ttt-r${i + 1}.ttt-c${j + 1}`).children();
+
+          // Make empty and go to next if this is empty
+          if (currentGameBoard.row[i].column[j] === "") {
+            imageBox.attr("alt", "");
+            imageBox.addClass("d-none");
+            continue;
+          }
+          imageBox.attr(
+            "src",
+            "/assets/game-demos/tic-tac-toe/" +
+              currentGameBoard.row[i].column[j] +
+              ".svg"
+          );
+          imageBox.attr("alt", currentGameBoard.row[i].column[j]);
+          imageBox.removeClass("d-none");
+        }
+      }
+    });
+}
+
+loadBoard();
