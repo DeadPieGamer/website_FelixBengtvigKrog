b49b7e3b8d98e56b0a19da7f5cfb6ed3a8ade2e5
DeadPieGamer <felix@krog.dk>
2025-06-12 18:42:44 +0200

    Updated some values

--- js/getHeaderAndFooter.js
+++ js/getHeaderAndFooter.js
@@ -1,4 +1,5 @@
 $("#header").load("/assets/decorations/topNavbar.html", highlightActivePage);
+$("#header").addClass("my-5");
 $("#footer").load("/assets/decorations/footer.html");
 
 // alert($(`.onpage-${$(document).attr("title")}`));
